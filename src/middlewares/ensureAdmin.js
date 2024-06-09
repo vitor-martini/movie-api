53146e2b1303eb2e32a5e975c436a4feb8a2b919
Vitor Martini <vitor-martini@outlook.com>
2024-06-09 13:01:49 -0300

    feat: ensure admin middleware

---
+++ src/middlewares/ensureAdmin.js
@@ -0,0 +1,16 @@
+const knex = require("../database/knex/index.js")
+const AppError = require("../utils/AppError");
+
+async function ensureAdmin(request, response, next) {
+  const { id: userId } = request.user
+
+  const user = await knex("users").where({ id: userId }).first()
+  console.log(user)
+  if(user.admin != 1) {
+    throw new AppError("User is not an admin")
+  }
+
+  return next()
+}
+
+module.exports = ensureAdmin;
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const MovieController = require("../controllers/MovieController.js");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated.js");
+const ensureAdmin = require("../middlewares/ensureAdmin.js");
 const multer = require("multer");
 const uploadConfig = require("../configs/upload.js");
 const CoverController = require("../controllers/CoverController.js");
@@ -11,10 +12,11 @@ const coverController = new CoverController();
 const upload = multer(uploadConfig.MULTER)
 
 moviesRoutes.use(ensureAuthenticated)
-moviesRoutes.post("/", moviesController.create);
-moviesRoutes.put("/:id", moviesController.update);
-moviesRoutes.get("/:id", moviesController.show);
+moviesRoutes.post("/", ensureAuthenticated, ensureAdmin, moviesController.create);
+moviesRoutes.put("/:id", ensureAuthenticated, ensureAdmin, moviesController.update);
+moviesRoutes.get("/:id", moviesController.show); 
+moviesRoutes.delete("/:id", ensureAuthenticated, ensureAdmin, moviesController.delete);
 moviesRoutes.get("/", moviesController.index);
-moviesRoutes.patch("/cover/:id", ensureAuthenticated, upload.single("cover"), coverController.update)
+moviesRoutes.patch("/cover/:id", ensureAuthenticated, ensureAdmin, upload.single("cover"), coverController.update)
 
 module.exports = moviesRoutes;
